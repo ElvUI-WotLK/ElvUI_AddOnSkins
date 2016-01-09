f1dc344174ff0dd8b9b205c8bb2f577c05177dbc
Bunny67 <zolov67@yandex.ru>
2016-01-09 14:55:22 +0300

    ...

--- addons/flightMap.lua
+++ addons/flightMap.lua
@@ -14,7 +14,7 @@ function addon:FlightMap()
 	
 	local S = E:GetModule("Skins");
 	local base = "InterfaceOptionsFlightMapPanel";
-    for optid, option in pairs(FLIGHTMAP_OPTIONS) do
+	for optid, option in pairs(FLIGHTMAP_OPTIONS) do
 		S:HandleCheckBox(_G[base .. "Option" .. optid]);
 	end
 end
