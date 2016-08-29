9b7cab1af2ecb5b551cfccdc6767f01e9f43bd7a
Crum <stschleyer89@gmail.com>
2016-08-28 23:30:32 -0500

    Spy Skin Update

--- addons/spy.lua
+++ addons/spy.lua
@@ -4,8 +4,16 @@ local addon = E:GetModule("AddOnSkins");
 if(not addon:CheckAddOn("Spy")) then return; end
 
 function addon:Spy()
-	Spy_MainWindow:StripTextures();
-	Spy_MainWindow:SetTemplate("Transparent");
+	local S = E:GetModule("Skins");
+
+		Spy_MainWindow:StripTextures();
+		Spy_MainWindow:SetTemplate("Transparent");
+
+		Spy_AlertWindow:StripTextures();
+		Spy_AlertWindow:SetTemplate("Transparent");
+		Spy_AlertWindow:Point('TOP', UIParent, 'TOP', 0, -130)
+
+		S:HandleCloseButton(Spy_MainWindow.CloseButton);
 end
 
 addon:RegisterSkin("Spy", addon.Spy);
