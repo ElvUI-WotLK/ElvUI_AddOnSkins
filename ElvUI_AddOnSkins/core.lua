23ecbbca65173deddfd73180453bbdf5788d1158
Pinya <800pin.ru@gmail.com>
2017-03-27 00:30:22 +0300

    cleanup

--- ElvUI_AddOnSkins/core.lua
+++ ElvUI_AddOnSkins/core.lua
@@ -1,17 +1,88 @@
 local addonName = ...;
 local E, L, V, P, G, _ = unpack(ElvUI);
 local EP = LibStub("LibElvUIPlugin-1.0", true);
-local addon = E:NewModule("AddOnSkins", "AceHook-3.0", "AceEvent-3.0");
+local AS = E:NewModule("AddOnSkins", "AceHook-3.0", "AceEvent-3.0");
 
-local find = string.find;
-local match = string.match;
-local trim = string.trim;
+local find, lower, match, trim = string.find, string.lower, string.match, string.trim
+local tinsert = table.insert
 
-addon.addOns = {};
+local GetAddOnInfo = GetAddOnInfo
+
+local addonList = {
+	"Omen",
+	"Recount",
+	"SexyCooldown",
+	"DBM",
+	"Skada",
+	"Auctionator",
+	"BugSack",
+	"CallToArms",
+	"Postal",
+	"QuestPointer",
+	"Clique",
+	"FloAspectBar",
+	"FloTotemBar",
+	"Spy",
+	"AtlasLoot",
+	"Atlas",
+	"FlightMap",
+	"WeakAuras",
+	"Overachiever",
+	"OpenGF",
+	"KHunterTimers",
+	"TellMeWhen",
+	"GearScore",
+	"AllStats",
+	"BlackList",
+	"GnomishVendorShrinker",
+	"ACP",
+	"EveryQuest",
+	"_NPCScan",
+	"MoveAnything",
+	"VanasKoS",
+	"BindPad",
+	"ZygorGuidesViewer",
+	"ZygorTalentAdvisor",
+	"WowLua",
+	"ChatBar",
+	"Skillet",
+	"TotemTimers",
+	"PlateBuffs",
+	"MageNuggets",
+	"InspectEquip",
+	"AdvancedTradeSkillWindow",
+	"AtlasQuest",
+	"AckisRecipeList",
+	"LightHeaded",
+	"Carbonite",
+	"Enchantrix",
+	"FishingBuddy",
+	"Talented",
+	"TinyPad",
+	"ZOMGBuffs",
+	"BuyEmAll",
+}
+
+AS.addOns = {};
 
 for i = 1, GetNumAddOns() do
 	local name, _, _, enabled = GetAddOnInfo(i);
-	addon.addOns[strlower(name)] = enabled ~= nil;
+	AS.addOns[lower(name)] = enabled ~= nil;
+end
+
+function AS:CheckAddOn(addon)
+	return self.addOns[lower(addon)] or false;
+end
+
+function AS:RegisterAddonOption(AddonName, options)
+	if select(6, GetAddOnInfo(AddonName)) == "MISSING" then return end
+
+	options.args.addOns.args[AddonName] = {
+		type = "toggle",
+		name = AddonName,
+		desc = L["TOGGLESKIN_DESC"],
+		hidden = function() return not self:CheckAddOn(AddonName) end
+	}
 end
 
 local positionValues = {
@@ -27,17 +98,6 @@ local positionValues = {
 };
 
 local function getOptions()
-	local function GenerateOptionTable(skinName, order)
-		local text = trim(skinName:gsub("^Blizzard_(.+)","%1"):gsub("(%l)(%u%l)","%1 %2"));
-		local options = {
-			type = "toggle",
-			order = order,
-			name = text,
-			desc = L["TOGGLESKIN_DESC"],
-		};
-		return options;
-	end
-
 	local options = {
 		order = 100,
 		type = "group",
@@ -50,308 +110,7 @@ local function getOptions()
 				guiInline = true,
 				get = function(info) return E.private.addOnSkins[info[#info]]; end,
 				set = function(info, value) E.private.addOnSkins[info[#info]] = value; E:StaticPopup_Show("PRIVATE_RL"); end,
-				args = {
-					_NPCScan = {
-						type = "toggle",
-						name = "_NPCScan",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("_NPCScan"); end
-					},
-					ACP = {
-						type = "toggle",
-						name = "ACP",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("ACP"); end
-					},
-					Omen = {
-						type = "toggle",
-						name = "Omen",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("Omen"); end
-					},
-					Recount = {
-						type = "toggle",
-						name = "Recount",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("Recount"); end
-					},
-					SexyCooldown = {
-						type = "toggle",
-						name = "SexyCooldown",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("SexyCooldown"); end
-					},
-					Skada = {
-						type = "toggle",
-						name = "Skada",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("Skada"); end
-					},
-					Auctionator = {
-						type = "toggle",
-						name = "Auctionator",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("Auctionator"); end
-					},
-					BugSack = {
-						type = "toggle",
-						name = "BugSack",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("BugSack"); end
-					},
-					CallToArms = {
-						type = "toggle",
-						name = "CallToArms",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("CallToArms"); end
-					},
-					Postal = {
-						type = "toggle",
-						name = "Postal",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("Postal"); end
-					},
-					QuestPointer = {
-						type = "toggle",
-						name = "QuestPointer",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("QuestPointer"); end
-					},
-					Clique = {
-						type = "toggle",
-						name = "Clique",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("Clique"); end
-					},
-					DBM = {
-						type = "toggle",
-						name = "DBM",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("DBM-Core"); end
-					},
-					FloAspectBar = {
-						type = "toggle",
-						name = "FloAspectBar",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("FloAspectBar"); end
-					},
-					FloTotemBar = {
-						type = "toggle",
-						name = "FloTotemBar",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("FloTotemBar"); end
-					},
-					Spy = {
-						type = "toggle",
-						name = "Spy",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("Spy"); end
-					},
-					AtlasLoot = {
-						type = "toggle",
-						name = "AtlasLoot",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("AtlasLoot"); end
-					},
-					Atlas = {
-						type = "toggle",
-						name = "Atlas",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("Atlas"); end
-					},
-					FlightMap = {
-						type = "toggle",
-						name = "FlightMap",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("FlightMap"); end
-					},
-					WeakAuras = {
-						type = "toggle",
-						name = "WeakAuras",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("WeakAuras"); end
-					},
-					Overachiever = {
-						type = "toggle",
-						name = "Overachiever",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("Overachiever"); end
-					},
-					OpenGF = {
-						type = "toggle",
-						name = "OpenGF",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("OpenGF"); end
-					},
-					KHunterTimers = {
-						type = "toggle",
-						name = "KHunterTimers",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("KHunterTimers"); end
-					},
-					TellMeWhen = {
-						type = "toggle",
-						name = "TellMeWhen",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("TellMeWhen"); end
-					},
-					GearScore = {
-						type = "toggle",
-						name = "GearScore",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("GearScore"); end
-					},
-					AllStats = {
-						type = "toggle",
-						name = "AllStats",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("AllStats"); end
-					},
-					BlackList = {
-						type = "toggle",
-						name = "BlackList",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("BlackList"); end
-					},
-					GnomishVendorShrinker = {
-						type = "toggle",
-						name = "GnomishVendorShrinker",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("GnomishVendorShrinker"); end
-					},
-					EveryQuest = {
-						type = "toggle",
-						name = "EveryQuest",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("EveryQuest"); end
-					},
-					MoveAnything = {
-						type = "toggle",
-						name = "MoveAnything",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("MoveAnything"); end
-					},
-					VanasKoS = {
-						type = "toggle",
-						name = "VanasKoS",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("VanasKoS"); end
-					},
-					BindPad = {
-						type = "toggle",
-						name = "BindPad",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("BindPad"); end
-					},
-					ZygorGuidesViewer = {
-						type = "toggle",
-						name = "ZygorGuidesViewer",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("ZygorGuidesViewer"); end
-					},
-					ZygorTalentAdvisor = {
-						type = "toggle",
-						name = "ZygorTalentAdvisor",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("ZygorTalentAdvisor"); end
-					},
-					WowLua = {
-						type = "toggle",
-						name = "WowLua",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("WowLua"); end
-					},
-					ChatBar = {
-						type = "toggle",
-						name = "ChatBar",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("ChatBar"); end
-					},
-					PlateBuffs = {
-						type = "toggle",
-						name = "PlateBuffs",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("PlateBuffs"); end
-					},
-					MageNuggets = {
-						type = "toggle",
-						name = "MageNuggets",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("MageNuggets"); end
-					},
-					InspectEquip = {
-						type = "toggle",
-						name = "InspectEquip",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("InspectEquip"); end
-					},
-					AdvancedTradeSkillWindow = {
-						type = "toggle",
-						name = "AdvancedTradeSkillWindow",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("AdvancedTradeSkillWindow"); end
-					},
-					AtlasQuest = {
-						type = "toggle",
-						name = "AtlasQuest",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("AtlasQuest"); end
-					},
-					AckisRecipeList = {
-						type = "toggle",
-						name = "AckisRecipeList",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("AckisRecipeList"); end
-					},
-					LightHeaded = {
-						type = "toggle",
-						name = "LightHeaded",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("LightHeaded"); end
-					},
-					Carbonite = {
-						type = "toggle",
-						name = "Carbonite",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("Carbonite"); end
-					},
-					Enchantrix = {
-						type = "toggle",
-						name = "Enchantrix",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("Enchantrix"); end
-					},
-					FishingBuddy = {
-						type = "toggle",
-						name = "FishingBuddy",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("FishingBuddy"); end
-					},
-					Talented = {
-						type = "toggle",
-						name = "Talented",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("Talented"); end
-					},
-					TinyPad = {
-						type = "toggle",
-						name = "TinyPad",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("TinyPad"); end
-					},
-					ZOMGBuffs = {
-						type = "toggle",
-						name = "ZOMGBuffs",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("ZOMGBuffs"); end
-					},
-					BuyEmAll = {
-						type = "toggle",
-						name = "BuyEmAll",
-						desc = L["TOGGLESKIN_DESC"],
-						hidden = function() return not addon:CheckAddOn("BuyEmAll"); end
-					},
-				}
+				args = {}
 			},
 			blizzard = {
 				order = 2,
@@ -380,7 +139,7 @@ local function getOptions()
 						name = L["Skada"],
 						get = function(info) return E.db.addOnSkins[info[#info]]; end,
 						set = function(info, value) E.db.addOnSkins[info[#info]] = value; Skada:ApplySettings(); end,
-						disabled = function() return not addon:CheckAddOn("Skada"); end,
+						disabled = function() return not AS:CheckAddOn("Skada"); end,
 						args = {
 							skadaTemplate = {
 								order = 1,
@@ -396,7 +155,7 @@ local function getOptions()
 								order = 2,
 								type = "toggle",
 								name = L["Template Gloss"],
-								disabled = function() return E.db.addOnSkins.skadaTemplate ~= "Default" or not addon:CheckAddOn("Skada"); end
+								disabled = function() return E.db.addOnSkins.skadaTemplate ~= "Default" or not AS:CheckAddOn("Skada"); end
 							},
 							spacer = {
 								order = 3,
@@ -417,7 +176,7 @@ local function getOptions()
 								order = 5,
 								type = "toggle",
 								name = L["Title Template Gloss"],
-								disabled = function() return E.db.addOnSkins.skadaTitleTemplate ~= "Default" or not addon:CheckAddOn("Skada"); end
+								disabled = function() return E.db.addOnSkins.skadaTitleTemplate ~= "Default" or not AS:CheckAddOn("Skada"); end
 							}
 						}
 					},
@@ -427,7 +186,7 @@ local function getOptions()
 						name = L["DBM"],
 						get = function(info) return E.db.addOnSkins[info[#info]]; end,
 						set = function(info, value) E.db.addOnSkins[info[#info]] = value; DBM.Bars:ApplyStyle(); DBM.BossHealth:UpdateSettings(); end,
-						disabled = function() return not addon:CheckAddOn("DBM-Core"); end,
+						disabled = function() return not AS:CheckAddOn("DBM-Core"); end,
 						args = {
 							dbmBarHeight = {
 								order = 1,
@@ -468,7 +227,7 @@ local function getOptions()
 						name = L["WeakAuras"],
 						get = function(info) return E.db.addOnSkins[info[#info]]; end,
 						set = function(info, value) E.db.addOnSkins[info[#info]] = value; E:StaticPopup_Show("PRIVATE_RL"); end,
-						disabled = function() return not addon:CheckAddOn("WeakAuras"); end,
+						disabled = function() return not AS:CheckAddOn("WeakAuras"); end,
 						args = {
 							weakAuraAuraBar = {
 								order = 1,
@@ -488,7 +247,7 @@ local function getOptions()
 						name = L["ChatBar"],
 						get = function(info) return E.db.addOnSkins[info[#info]]; end,
 						set = function(info, value) E.db.addOnSkins[info[#info]] = value; ChatBar_UpdateButtonOrientation(); ChatBar_UpdateButtons(); end,
-						disabled = function() return not addon:CheckAddOn("ChatBar"); end,
+						disabled = function() return not AS:CheckAddOn("ChatBar"); end,
 						args = {
 							chatBarSize = {
 								order = 1,
@@ -532,8 +291,8 @@ local function getOptions()
 				order = 4,
 				type = "group",
 				name = "Embed Settings",
-				get = function(info) return E.db.addOnSkins.embed[ info[#info] ] end,
-				set = function(info, value) E.db.addOnSkins.embed[ info[#info] ] = value; E:GetModule("EmbedSystem"):Check() end,
+				get = function(info) return E.db.addOnSkins.embed[info[#info]] end,
+				set = function(info, value) E.db.addOnSkins.embed[info[#info]] = value; E:GetModule("EmbedSystem"):Check() end,
 				args = {
 					desc = {
 						order = 1,
@@ -602,15 +361,15 @@ local function getOptions()
 		},
 	}
 
+	for _, addonName in pairs(addonList) do
+		AS:RegisterAddonOption(addonName, options)
+	end
+
 	E.Options.args.addOnSkins = options;
 end
 
-function addon:CheckAddOn(addon)
-	return self.addOns[strlower(addon)] or false;
-end
-
-function addon:Initialize()
+function AS:Initialize()
 	EP:RegisterPlugin(addonName, getOptions);
 end
 
-E:RegisterModule(addon:GetName());
+E:RegisterModule(AS:GetName());
