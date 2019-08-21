2efe98bbffd90b168e51b8f1048166deaf851b73
Apollyonn <jellefenxd@gmail.com>
2019-08-21 21:10:11 +0300

    update postal

--- ElvUI_AddOnSkins/Skins/Addons/postal.lua
+++ ElvUI_AddOnSkins/Skins/Addons/postal.lua
@@ -1,60 +1,88 @@
-local E, L, V, P, G, _ = unpack(ElvUI);
-local S = E:GetModule("Skins");
+local E, L, V, P, G = unpack(ElvUI)
+local S = E:GetModule("Skins")
 
 local function LoadSkin()
-	if(not E.private.addOnSkins.Postal) then return; end
+	if not E.private.addOnSkins.Postal then return end
 
 	for i = 1, INBOXITEMS_TO_DISPLAY do
-		local item = _G["MailItem" .. i .. "ExpireTime"];
-		if(item) then
-			item:SetPoint("TOPRIGHT", "MailItem" .. i, "TOPRIGHT", -5, -10);
-			if(item.returnicon) then
-				item.returnicon:Point("TOPRIGHT", item, "TOPRIGHT", 20, 0);
+		local mail = _G["MailItem"..i]
+		local button = _G["MailItem"..i.."Button"]
+		local expire = _G["MailItem"..i.."ExpireTime"]
+		local inboxCB = _G["PostalInboxCB"..i]
+
+		button:SetScale(1)
+		if i == 1 then
+			mail:Point("TOPLEFT", 38, -80)
+		end
+
+		if expire then
+			expire:Point("TOPRIGHT", mail, "TOPRIGHT", 3, -2)
+
+			if expire.returnicon then
+				expire.returnicon:StripTextures(true)
+				S:HandleCloseButton(expire.returnicon)
+				expire.returnicon:Point("TOPRIGHT", expire, "TOPRIGHT", 25, -3)
+				expire.returnicon:Size(26)
 			end
 		end
-		if(_G["PostalInboxCB" .. i]) then
-			S:HandleCheckBox(_G["PostalInboxCB" .. i]);
+
+		if inboxCB then
+			S:HandleCheckBox(inboxCB)
+			inboxCB:Point("RIGHT", mail, "LEFT", 5, -5)
 		end
 	end
 
 	if PostalSelectOpenButton then
-		S:HandleButton(PostalSelectOpenButton, true);
-	end
-
-	if Postal_OpenAllMenuButton then
-		S:HandleNextPrevButton(Postal_OpenAllMenuButton);
-	end
-
-	if PostalOpenAllButton then
-		S:HandleButton(PostalOpenAllButton, true);
+		S:HandleButton(PostalSelectOpenButton, true)
+		PostalSelectOpenButton:ClearAllPoints()
+		PostalSelectOpenButton:Point("TOPLEFT", InboxFrame, "TOPLEFT", 45, -45)
 	end
 
 	if PostalSelectReturnButton then
-		S:HandleButton(PostalSelectReturnButton, true);
+		S:HandleButton(PostalSelectReturnButton, true)
+		PostalSelectReturnButton:ClearAllPoints()
+		PostalSelectReturnButton:Point("LEFT", PostalSelectOpenButton, "RIGHT", 36, 0)
+	end
+
+	if Postal_OpenAllMenuButton then
+		S:HandleNextPrevButton(Postal_OpenAllMenuButton)
+		Postal_OpenAllMenuButton:ClearAllPoints()
+		Postal_OpenAllMenuButton:Point("LEFT", PostalOpenAllButton, "RIGHT", 2, 0)
+		Postal_OpenAllMenuButton:Size(25)
+	end
+
+	if PostalOpenAllButton then
+		S:HandleButton(PostalOpenAllButton, true)
 	end
 
 	if Postal_ModuleMenuButton then
-		S:HandleNextPrevButton(Postal_ModuleMenuButton);
-		Postal_ModuleMenuButton:SetPoint("TOPRIGHT", MailFrame, -60, -16);
+		S:HandleNextPrevButton(Postal_ModuleMenuButton, nil, nil, true)
+		Postal_ModuleMenuButton:Point("TOPRIGHT", MailFrame, -60, -12)
+		Postal_ModuleMenuButton:Size(26)
 	end
 
 	if Postal_BlackBookButton then
-		S:HandleNextPrevButton(Postal_BlackBookButton);
+		S:HandleNextPrevButton(Postal_BlackBookButton)
+		Postal_BlackBookButton:ClearAllPoints()
+		Postal_BlackBookButton:Point("LEFT", SendMailNameEditBox, "RIGHT", 2, 0)
+		Postal_BlackBookButton:Size(20)
 	end
 
 	hooksecurefunc(Postal, "CreateAboutFrame", function()
 		if PostalAboutFrame then
-			PostalAboutFrame:StripTextures();
-			PostalAboutFrame:SetTemplate("Transparent");
+			PostalAboutFrame:StripTextures()
+			PostalAboutFrame:SetTemplate("Transparent")
+
 			if PostalAboutScroll then
-				S:HandleScrollBar(PostalAboutScrollScrollBar);
+				S:HandleScrollBar(PostalAboutScrollScrollBar)
 			end
-			local closeButton = select(2, PostalAboutFrame:GetChildren());
+
+			local closeButton = select(2, PostalAboutFrame:GetChildren())
 			if closeButton then
-				S:HandleCloseButton(closeButton);
+				S:HandleCloseButton(closeButton)
 			end
 		end
-	end);
+	end)
 end
 
-S:AddCallbackForAddon("Postal", "Postal", LoadSkin);
+S:AddCallbackForAddon("Postal", "Postal", LoadSkin)
