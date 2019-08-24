33ebf41239ce424eb9a1380b3c35cfe25ebdd57e
Pinya <800pin.ru@gmail.com>
2019-08-24 23:12:18 +0300

    Update trinketMenu.lua
    close #91

--- ElvUI_AddOnSkins/Skins/Addons/trinketMenu.lua
+++ ElvUI_AddOnSkins/Skins/Addons/trinketMenu.lua
@@ -10,14 +10,34 @@ local _G = _G
 local function LoadSkin()
 	if not E.private.addOnSkins.TrinketMenu then return end
 
-	TrinketMenu_MainFrame:SetTemplate("Transparent")
-	S:HandleItemButton(TrinketMenu_Trinket0)
-	S:HandleItemButton(TrinketMenu_Trinket1)
+	hooksecurefunc(TrinketMenu, "ReflectLock", function()
+		TrinketMenu_MainFrame:SetTemplate("Transparent")
+		TrinketMenu_MenuFrame:SetTemplate("Transparent")
+	end)
 
-	TrinketMenu_MenuFrame:SetTemplate("Transparent")
+	local AB = E:GetModule("ActionBars")
+
+	local function skinButton(button)
+		local name = button:GetName()
+		local cooldown = _G[name .. "Cooldown"]
+
+		AB:StyleButton(button)
+
+		cooldown.timer = E:CreateCooldownTimer(cooldown)
+		_G[name .. "Time"] = cooldown.timer.text
+	end
+
+--	TrinketMenu_TimersFrame:SetScript("OnUpdate", nil)
+	TrinketMenu.WriteWornCooldowns = E.noop
+	TrinketMenu.WriteMenuCooldowns = E.noop
+	TrinketMenu.WriteCooldown = E.noop
+
+	for i = 0, 1 do
+		skinButton(_G["TrinketMenu_Trinket" .. i])
+	end
 
 	for i = 1, 30 do
-		S:HandleItemButton(_G["TrinketMenu_Menu"..i])
+		skinButton(_G["TrinketMenu_Menu"..i])
 	end
 end
 
