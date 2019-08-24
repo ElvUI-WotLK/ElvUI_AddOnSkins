f7ba9262ba886c6d1758c91dc2700fd05261a7f0
Pinya <800pin.ru@gmail.com>
2019-08-24 16:23:38 +0300

    close #84

--- ElvUI_AddOnSkins/Skins/Addons/everyQuest.lua
+++ ElvUI_AddOnSkins/Skins/Addons/everyQuest.lua
@@ -1,51 +1,61 @@
-local E, L, V, P, G = unpack(ElvUI);
-local S = E:GetModule("Skins");
+local E, L, V, P, G = unpack(ElvUI)
+local S = E:GetModule("Skins")
 
 -- EveryQuest r162
+-- https://www.wowace.com/projects/everyquest/files/459624
 
 local function LoadSkin()
-	if(not E.private.addOnSkins.EveryQuest) then return; end
+	if not E.private.addOnSkins.EveryQuest then return end
 
-	local addon = LibStub("AceAddon-3.0"):GetAddon("EveryQuest", true);
+	local addon = LibStub("AceAddon-3.0"):GetAddon("EveryQuest", true)
 	if not addon then return end
 
-	EveryQuestFrame:StripTextures();
-	EveryQuestFrame:CreateBackdrop("Transparent");
-	EveryQuestFrame.backdrop:Point("TOPLEFT", 0, 0);
-	EveryQuestFrame.backdrop:Point("BOTTOMRIGHT", -22, 0);
+	EveryQuestFrame:SetHitRectInsets(0, 0, 0, 0)
+	EveryQuestFrame:StripTextures()
+	EveryQuestFrame:SetTemplate("Transparent")
 
-	EveryQuestListScrollFrame:CreateBackdrop("Default");
-	EveryQuestListScrollFrame:Point("TOPLEFT", EveryQuestFrame, "TOPLEFT", 20, -73)
-	S:HandleScrollBar(EveryQuestListScrollFrameScrollBar);
+	EveryQuestTitleText:Point("TOP", 0, -3)
 
-	S:HandleCloseButton(EveryQuestFrameCloseButton, EveryQuestFrame.backdrop);
+	EveryQuestFrame:SetSize(364, 512)
 
-	S:HandleButton(addon.frames.Show);
-	addon.frames.Show:Size(50, 20);
+	addon.frames.Menu:ClearAllPoints()
+	addon.frames.Menu:Point("TOP", EveryQuestFrame, 0, -20)
+	S:HandleDropDownBox(addon.frames.Menu, 190)
 
-	S:HandleDropDownBox(addon.frames.Menu);
-	addon.frames.Menu:Width(190);
+	EveryQuestListScrollFrame:SetTemplate("Transparent")
+	EveryQuestListScrollFrame:ClearAllPoints()
+	EveryQuestListScrollFrame:Point("TOPLEFT", EveryQuestFrame, 20, -73)
+	S:HandleScrollBar(EveryQuestListScrollFrameScrollBar)
 
-	S:HandleButton(addon.frames.Show);
-	S:HandleButton(addon.frames.Filters);
-	S:HandleButton(addon.frames.Options);
-	S:HandleButton(EveryQuestExitButton);
+	S:HandleCloseButton(EveryQuestFrameCloseButton, EveryQuestFrame)
 
-	EveryQuestExitButton:ClearAllPoints();
-	EveryQuestExitButton:Point("BOTTOMRIGHT", EveryQuestFrame, "BOTTOMRIGHT", -41, 4);
+	addon.frames.Show:ClearAllPoints()
+	addon.frames.Show:Point("BOTTOMLEFT", QuestLogCount, "TOPLEFT", 0, 3)
+	addon.frames.Show:Size(50, 20)
+	S:HandleButton(addon.frames.Show)
 
-	addon.frames.Options:ClearAllPoints();
-	addon.frames.Options:Point("TOPRIGHT", EveryQuestExitButton, "TOPLEFT", -2, 0);
+	S:HandleButton(EveryQuestExitButton)
+	S:HandleButton(addon.frames.Options)
+	S:HandleButton(addon.frames.Filters)
 
-	addon.frames.Filters:ClearAllPoints();
-	addon.frames.Filters:Point("TOPRIGHT", addon.frames.Options, "TOPLEFT", -2, 0);
+	EveryQuestExitButton:ClearAllPoints()
+	EveryQuestExitButton:Point("BOTTOMRIGHT", EveryQuestFrame, "BOTTOMRIGHT", -21, 4)
+
+	addon.frames.Options:ClearAllPoints()
+	addon.frames.Options:Point("RIGHT", EveryQuestExitButton, "LEFT", -1, 0)
+
+	addon.frames.Filters:ClearAllPoints()
+	addon.frames.Filters:Point("RIGHT", addon.frames.Options, "LEFT", -1, 0)
+
+	addon.frames.Shown:ClearAllPoints()
+	addon.frames.Shown:Point("BOTTOMLEFT", EveryQuestFrame, "BOTTOMLEFT", 20, 4)
 
 	hooksecurefunc(addon, "OnShow", function(self)
 		if not self.dbpc.profile.posx and not self.dbpc.profile.posy then
-			EveryQuestFrame:ClearAllPoints();
-			EveryQuestFrame:SetPoint("TOPLEFT", QuestLogFrame, "TOPRIGHT", 10, -12)
+			EveryQuestFrame:ClearAllPoints()
+			EveryQuestFrame:Point("TOPLEFT", QuestLogFrame, "TOPRIGHT", -2, -12)
 		end
 	end)
 end
 
-S:AddCallbackForAddon("EveryQuest", "EveryQuest", LoadSkin);
+S:AddCallbackForAddon("EveryQuest", "EveryQuest", LoadSkin)
