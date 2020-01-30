d52ca46e4417d1fb93b88a8c43c50f2b8dc83a0a
Pinya <800pin.ru@gmail.com>
2020-01-30 17:21:23 +0300

    Update allStats.lua

--- ElvUI_AddOnSkins/Skins/Addons/allStats.lua
+++ ElvUI_AddOnSkins/Skins/Addons/allStats.lua
@@ -1,6 +1,9 @@
 local E, L, V, P, G = unpack(ElvUI)
 local S = E:GetModule("Skins")
 
+-- All Stats 1.1
+-- https://www.curseforge.com/wow/addons/all-stats/files/430951
+
 local function LoadSkin()
 	if not E.private.addOnSkins.AllStats then return end
 
@@ -8,6 +11,9 @@ local function LoadSkin()
 	AllStatsFrame:SetTemplate("Transparent")
 	AllStatsFrame:Height(424)
 	AllStatsFrame:Point("TOPLEFT", PaperDollFrame, "TOPLEFT", 351, -12)
+
+	AllStatsButtonShowFrame:Height(21)
+	AllStatsButtonShowFrame:Point("BOTTOMRIGHT", -40, 84)
 	S:HandleButton(AllStatsButtonShowFrame)
 end
 
