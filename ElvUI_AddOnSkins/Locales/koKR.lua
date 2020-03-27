fea031b2b3a2b92653437f3214d020167eba7fb8
Pinya <800pin.ru@gmail.com>
2020-03-27 10:37:36 +0300

    add missed locale files

---
+++ ElvUI_AddOnSkins/Locales/koKR.lua
@@ -0,0 +1,22 @@
+local E = unpack(ElvUI); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
+local L = E.Libs.ACL:NewLocale("ElvUI", "koKR")
+
+L["AddOn Skins"] = true
+L["AuraBar Backdrop"] = true
+L["Blizzard Skins"] = true
+L["Default"] = true
+L["Double"] = true
+L["Embed Type"] = true
+L["Font Size"] = true
+L["Icon Cooldown"] = true
+L["Left Panel"] = true
+L["Left Window Width"] = true
+L["Misc Options"] = true
+L["None"] = true
+L["Right Panel"] = true
+L["Single"] = true
+L["Template Gloss"] = true
+L["Template"] = true
+L["Title Template Gloss"] = true
+L["Title Template"] = true
+L["Toggle Embedded Addon"] = true
