fae3c2cf0261317fc6a02f6580578cfe42528d19
Crum <stschleyer89@gmail.com>
2020-08-02 17:24:47 -0500

    Update FloAspecBar skin

--- ElvUI_AddOnSkins/Skins/Addons/floAspectBar.lua
+++ ElvUI_AddOnSkins/Skins/Addons/floAspectBar.lua
@@ -8,12 +8,37 @@ if not AS:IsAddonLODorEnabled("FloAspectBar") then return end
 -- FloAspectBar 3.3.0.16
 -- https://www.curseforge.com/wow/addons/flo-aspect-bar/files/399320
 
-S:AddCallbackForAddon("FloAspectBar", "FloAspectBar", function()
+S:AddCallbackForAddon("FloAspectBar", "FloAspectBar", function(frame)
 	if not E.private.addOnSkins.FloAspectBar then return end
 
-	FloAspectBar:SetTemplate("Default")
+	if FLO_CLASS_NAME == "HUNTER" then
+		local frame = _G[frame]
+		FloLib_ShowBorders(frame)
 
-	for i = 1, 10 do
-		AB:StyleButton(_G["FloAspectBarButton" .. i])
+		local function toggleBorders(self)
+			if (self == frame) and self.globalSettings.borders then
+				self:SetTemplate("Transparent")
+				if self.settings and self.settings.color then
+					self:SetBackdropBorderColor(unpack(self.settings.color))
+				end
+			else
+				FloLib_HideBorders(self)
+			end
+		end
+
+		if not S:IsHooked("FloLib_ShowBorders") then
+			S:SecureHook("FloLib_ShowBorders", toggleBorders)
+		end
+
+		frame:SetClampedToScreen(true)
+
+		for i = 1, frame:GetNumChildren() do
+			local child = select(i, frame:GetChildren())
+			if child then
+				if child:IsObjectType("CheckButton") then
+					AB:StyleButton(child)
+				end
+			end
+		end
 	end
 end)
