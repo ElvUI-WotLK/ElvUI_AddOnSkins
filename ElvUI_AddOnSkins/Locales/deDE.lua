3046a6541de96a323b46d9d654fc60adeee04709
Zidras <10605951+Zidras@users.noreply.github.com>
2021-06-15 22:31:07 +0100

    DBM Half-bar Skin

--- ElvUI_AddOnSkins/Locales/deDE.lua
+++ ElvUI_AddOnSkins/Locales/deDE.lua
@@ -3,6 +3,7 @@ local L = E.Libs.ACL:NewLocale("ElvUI", "deDE")
 
 L["AddOn Skins"] = true
 L["AuraBar Backdrop"] = true
+L["DBM Half-bar Skin"] = "DBM Halbeleisten Skin"
 L["Default"] = true
 L["Double"] = true
 L["Embed Type"] = true
